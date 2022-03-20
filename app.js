3b4d72f6f45cef3fd81c370aa79b28f027593ecb
Manan Doshi <manandoshi1301@gmail.com>
2022-03-20 13:02:37 +0530

    signup signin routes added

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send("Hello from server!");
 })
 
+app.get('/signin', (req, res) => {
+  res.send("Hello to signin from server!");
+})
+
+app.get('/signup', (req, res) => {
+  res.send("Hello to signup from server!");
+})
+
 app.listen(3000, () => {
   console.log("Server is running!"); ``
 })
