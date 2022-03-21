e806aad35a02743a960af5bb5be28ff4f072f502
Manan Doshi <manandoshi1301@gmail.com>
2022-03-22 00:03:53 +0530

    Converted routes into auth file

--- server/app.js
+++ server/app.js
@@ -9,16 +9,26 @@ dotenv.config({ path: './config.env' });
 const PORT = process.env.PORT;
 require("./db/conn.js");
 
+app.use(express.json());
+
 // const User = require("./models/userSchema");
 
 
-app.get('/', (req, res) => {
-  res.send("Hello from server!");
-})
 
-app.get('/signin', (req, res) => {
-  res.send("Hello to signin from server!");
-})
+app.use(require('./router/auth'));
+
+// app.get('/', (req, res) => {
+//   res.send("Hello from server in app!");
+// })
+
+// app.get('/signin', (req, res) => {
+//   res.send("Hello to signin from server!");
+// })
+
+// app.post('/signin', (req, res) => {
+//   console.log(req.body);
+//   res.json({ message: req.body });
+// })
 
 app.get('/signup', (req, res) => {
   res.send("Hello to signup from server!");
