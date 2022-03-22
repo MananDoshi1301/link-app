236ef47e0c01328a02dc2dfc8c417bbe688e2e1b
Manan Doshi <manandoshi1301@gmail.com>
2022-03-22 22:31:57 +0530

    Configured query to post data to db

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,42 @@
+require('../db/conn');
 const express = require('express');
+const User = require('../models/userSchema');
 
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
   res.send("Hello from server in route!");
 })
 
-router.post('/signin', (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+
+router.post('/signup', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Plz fill all fields!" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "Email already exists!" });
+      }
+
+      const user = new User({ email, password });
+
+      user.save().then(() => {
+        res.status(201).json({ message: "User registered succesfully!" });
+      }).catch((err) => res.status(501).json({ error: err }));
+    }).catch(err => console.log(err));
 });
+
+// router.post('/signin', (req, res) => {
+//   const { email, password } = req.body;
+//   if (!email || !password) {
+//     return res.status(422).json({ error: "Plz fill all fields!" });
+//   }
+
+//   User.findOne({ email: email });
+// });
 
 module.exports = router;
