642732a485acb8a8b702b661745fe36feabebdb2
Manan Doshi <manandoshi1301@gmail.com>
2022-03-23 00:55:49 +0530

    Password hash and save cmplete using pre on save

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -10,6 +11,14 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 })
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+})
+
 
 const User = mongoose.model('USER_DETS', userSchema);
 
