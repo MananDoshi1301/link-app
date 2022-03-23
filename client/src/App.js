e05920191abea39be65b5eb00990efe8b42c98fb
Manan Doshi <manandoshi1301@gmail.com>
2022-03-24 02:19:15 +0530

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './components/Authentication/Signin';
 import Signup from './components/Authentication/Signup';
+import Navbar from './components/common/Navbar';
+import Home from './components/Home';
 
 function App() {
 
   const [details, setDetails] = useState({
-    email: "", id: ""
+    email: "user", id: ""
   })
 
   return (
     <div className="App">
-      {/* <Signup /> */}
-      <Signin />
+      <Router>
+        <Navbar details={details} />
+        <Routes>
+          <Route path={'/'} element={<Home />}>
+          </Route>
+          <Route path={'/signup'} element={<Signup />}>
+          </Route>
+          <Route path={'/signin'} element={<Signin />}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
