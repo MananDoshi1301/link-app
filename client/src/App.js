d21d9bef3ae97710555ad0d910ec2d737f4d8e92
Manan Doshi <manandoshi1301@gmail.com>
2022-03-23 15:09:22 +0530

    Added signin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Signin from './components/Authentication/Signin';
 import Signup from './components/Authentication/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      {/* <Signup /> */}
+      <Signin />
     </div>
   );
 }
