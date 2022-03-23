f533f07ee8db1c28f43d2228aaf56e29de0fc665
Manan Doshi <manandoshi1301@gmail.com>
2022-03-23 15:25:57 +0530

    Created global state prop

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Signin from './components/Authentication/Signin';
 import Signup from './components/Authentication/Signup';
 
 function App() {
+
+  const [details, setDetails] = useState({
+    email: "", id: ""
+  })
+
   return (
     <div className="App">
       {/* <Signup /> */}
